905964d841aa8c758ce6dad25c9eeb326fd15b52
gamzeadbll <167094170+gamzeadbll@users.noreply.github.com>
2025-01-05 11:22:13 +0300

    Update combineExcels.py
    combine excel files as All Data

--- utils/combineExcels.py
+++ utils/combineExcels.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 21 17:22:28 2024
-
-@author: gamzeadibelli
-"""
-
 import os
 import pandas as pd
 
-directory = "/Users/gamzeadibelli/OZU DS/CS552/Project/data"
+directory = "C:/Users/z004417p/OneDrive - Siemens AG/OneDrive/Masaüstü/CS552/google_data"
+university_data = "C:/Users/z004417p/OneDrive - Siemens AG/OneDrive/Masaüstü/CS552/universities.xlsx"
+
+universities_data = pd.read_excel(university_data)
 dataframes = []
 
 for file in os.listdir(directory):
-    if "kadro" in file.lower() and file.endswith(".xlsx"):  # Check if 'kadro' is in the filename and it is an Excel file
+    if  file.endswith(".xlsx"):
         filepath = os.path.join(directory, file)
-        # Read the Excel file into a DataFrame
+        
         df = pd.read_excel(filepath)
+        university_name = file.replace('_authors.xlsx', '')
+        df['University'] = university_name
         dataframes.append(df)
 
 combined_df = pd.concat(dataframes, ignore_index=True)
+final_df = combined_df.merge(universities_data, on='University', how='left')
 
-output_file = os.path.join(directory, "combined.xlsx")
-combined_df.to_excel(output_file, index=False)
+output_file = "C:/Users/z004417p/OneDrive - Siemens AG/OneDrive/Masaüstü/CS552/All_Data.xlsx"
+final_df.to_excel(output_file, index=False)
 
 print(f"Combined file saved as {output_file}")
